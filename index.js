1efd4a43e705dfb0cb1fcc315874dbbd1a9b2ace
vishnu2711S <svishnupriya2711@gmail.com>
2025-02-03 12:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ mongoose.connect('mongodb://localhost:27017/sample')
     .catch((err) => console.log(err))
 
 
-//app.get('/', (req, res)=>{
-  //res.send('server reacted...
+app.get('/', (req, res)=>{
+  res.send('server reacted...'})
